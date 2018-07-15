aeb34d138d47a1325abefe975fe75e2478b9e74c
Emile Larose-Levac <emile.larose@gmail.com>
2018-07-15 11:38:08 -0400

    Added the ability to pass a file as an argument or the program as an argument. Also added a few examples.

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Hello, World! in Brainfuck:
 The default cell size is 8 bits (can be changed with the typedef).
 The data array (tape) is 30000 cells long. Might make it dynamic in the future.
 
+## Usage
+You can pass a file as an argument (ex: ./Brainfuck "examples/helloWorld.bf"),
+or pass a program as an argument (ex: ./Brainfuck "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.")
+
--- main.cpp
+++ main.cpp
@@ -1,25 +1,40 @@
+#include <fstream>
 #include <iostream>
 #include <chrono>
 #include "BrainfuckInterpreter.hpp"
 
-int main() {
-    static const int MAX_EMPTY_LINES = 2; // Max empty lines in a pasted code
+int main(int argc, char* argv[]) {
+    // Trying to read a program from a file or an argument
     std::string program = "";
-
-    std::cout << "Paste your Brainfuck program here (press enter " << MAX_EMPTY_LINES << " times):\n\n";
-
-    std::string line;
-    int emptyLinesCounter = 0;
-    while(true){
-        getline(std::cin, line);
-        if(line.empty()){
-            emptyLinesCounter++;
-            if(emptyLinesCounter >= MAX_EMPTY_LINES) break;
-        } else emptyLinesCounter = 0;
-
-        program += line;
+    if(argc > 1){
+        std::ifstream bfFile;
+        bfFile.open(argv[1]);
+        if(bfFile.fail()){
+            program = argv[1];
+        }else{
+            std::string line;
+            while(getline(bfFile, line)){
+                program += line;
+            }
+        }
     }
+    else{
+        static const int MAX_EMPTY_LINES = 2; // Max empty lines in a pasted code
 
+        std::cout << "Paste your Brainfuck program here (press enter " << MAX_EMPTY_LINES << " times):\n\n";
+        std::string line;
+        int emptyLinesCounter = 0;
+        while(true){
+            getline(std::cin, line);
+            if(line.empty()){
+                emptyLinesCounter++;
+                if(emptyLinesCounter >= MAX_EMPTY_LINES) break;
+            } else emptyLinesCounter = 0;
+
+            program += line;
+        }
+    }
+    // Using the interpreter
     BrainfuckInterpreter interpreter(program);
 
     if(!interpreter.isValidProgram()){
